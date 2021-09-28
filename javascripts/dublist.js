067f20b7f4e8d8147f5f4d39b42eeafc6954455d
TroyL <troy.lochner@gmail.com>
2021-09-28 12:55:40 -0400

    try fixing cors

--- javascripts/dublist.js
+++ javascripts/dublist.js
@@ -9,7 +9,7 @@ var table = new Tabulator("#table-bordered", {
             "Accept": "application/json", //tell the server we need JSON back
             "X-Requested-With": "XMLHttpRequest", //fix to help some frameworks respond correctly to request
             "Content-type": 'application/json; charset=utf-8', //set the character encoding of the request
-            "Access-Control-Allow-Origin": "https://troylochner.github.io/", //the URL origin of the site making the request
+
         }},
     layout: "fitColumns",
     columnHeaderSortMulti: true,
